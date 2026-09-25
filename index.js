7e6e84baf2d664b6bc16afbd9ac76f8acb6f5dde
Stefany Abreu <maria.nunes380@aluno.ce.gov.br>
2026-09-24 22:02:08 -0300

    Add daily routine health check in index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const nome = "Minha rotina saudável"
+let agua = 5
+let frutas = 2
+let exercicio = 30
+let sono = 8
+let cafe = true
+let almoco = true
+let jantar = true
+
+console.log(nome)
+console.log("Bom dia!")
+
+if (agua >= 6) {
+  console.log("Você bebeu água suficiente")
+} else {
+  console.log("Beba mais água durante o dia")
+}
+
+if (frutas >= 2) {
+  console.log("Você comeu frutas hoje")
+} else {
+  console.log("Tente comer mais frutas")
+}
+
+if (exercicio >= 30) {
+  console.log("Você fez exercícios hoje")
+} else {
+  console.log("Faça um pouco de exercício")
+}
